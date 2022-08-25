0a1f303207d79080c599d55a1d35893219ffcbf7
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-08-25 09:32:16 -0400

    partially completed Post model

--- models/Post.js
+++ models/Post.js
@@ -4,10 +4,38 @@ const sequelize = require("../config/connection");
 // Creating our Post model
 class Post extends Model {}
 
-Post.init({
-  id: {
-    type: Datatypes.INTEGER,
-    allowNull: false,
-    primaryKey: false,
+Post.init(
+  {
+    id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      primaryKey: false,
+    },
+    title: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    post_url: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true,
+      },
+    },
+    user_id: {
+      type: Datatypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
   },
-});
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
