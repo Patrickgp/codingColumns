35c431038d31fa58afdfd706a0abead624ffe93e
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-08-27 19:55:23 -0400

    completed comment routes for CRUD operations

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+
+// GET /api/comments/
+router.get("/", (req, res) => {
+  Comment.findAll()
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST /api/comments/
+router.post("/", (req, res) => {
+  // check the session
+  if (req.session) {
+    Comment.create({
+      content: req.body.content,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id,
+    })
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+// DELETE /api/comments/:id
+router.delete("/:id", (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No comment found with this id" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
