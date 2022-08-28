415d6296056e0f3eeac9e5b47c3e575469ed5bc8
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-08-28 09:06:52 -0400

    added comment functionality still need to display comments on single post template

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -27,6 +27,46 @@ router.get("/", (req, res) => {
     });
 });
 
+// GET a Post by ID
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "post_url", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "content", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass the data to the template
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 // Get Login page route
 router.get("/login", (req, res) => {
   res.render("login");
